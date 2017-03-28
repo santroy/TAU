f1c15979def828d97e468ddf1526c3b553f96f25
Krystian Trojanowski <krytront@gmail.com>
2017-03-28 11:20:49 +0200

    Update Vector.java

--- lab1/src/main/java/pl/edu/pjatk/tau/Vector.java
+++ lab1/src/main/java/pl/edu/pjatk/tau/Vector.java
@@ -4,9 +4,9 @@ import java.util.List;
 
 public interface Vector {
     public Vector addVector(Vector vector);
-    public static Vector addVectors(Vector vector1, Vector vector2) {
+    /*public static Vector addVectors(Vector vector1, Vector vector2) {
 		return null;
-	}
+	}*/
     public void setVectorValues(List<Double> vector);
     public List<Double> getVectorValues();
 }
