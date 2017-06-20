60897a359170414deb40428c1f3dda7fd7af174a
santroy <krytront@gmail.com>
2017-06-20 11:24:26 +0200

    testyjunit

--- lab11/src/test/java/pl/tau/SpringAppTest.java
+++ lab11/src/test/java/pl/tau/SpringAppTest.java
@@ -7,7 +7,9 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import pl.tau.dao.ClientDao;
+import pl.tau.dao.OrderDao;
 import pl.tau.model.Client;
+import pl.tau.model.Order;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppContext.class})
@@ -18,12 +20,37 @@ public class SpringAppTest {
 	
 	@Autowired
 	private ClientDao clientDao;
+	@Autowired
+	OrderDao orderDao;
 	
 	
 	@Test
 	public void saveClientTest() {
-		Client client = new Client("Jan", "Kowalski", "Krakowskie przedmiescie 23, Warszawa");
+		Client client = new Client("Krystian", "Nowacki", "Gdansk Wolnosci 22/1");
 	    clientDao.save(client);
+	    Order order = new Order("MSI Notebook", "kurier");
+	    order.setClient(client);
+	    orderDao.save(order);
 	}
+	
+	@Test
+	public void removeClientTest() {
+		Client client2 = new Client("Marcin", "Kowalski", "Krakowskie przedmiescie 23, Warszawa");
+	    clientDao.save(client2);
+	    clientDao.remove(client2.getId());
+	}
+	
+	@Test
+	public void updateClientTest() {
+		Client client3 = new Client("Bartek", "Kowalski", "Wilanowskie pola 223, Bydgodszcz");
+	    clientDao.save(client3);
+	    Client client4 = new Client("Michal", "Kowalski", "Wilanowskie pola 223, Bydgodszcz");
+	    client4.setId(client3.getId());
+	    clientDao.update(client4);
+	     
+	    
+	}
+	
+
 
 }
